70c6c113730c4f5f40a7f5cb517863d1b4dd2eac
Fran <kezicfran@gmail.com>
2023-05-09 01:16:44 +0300

    build works

--- src/components/WordMatrix.tsx
+++ src/components/WordMatrix.tsx
@@ -1,4 +1,3 @@
-import * as classnames from 'classnames';
 
 interface WordMatrixProps {
   word: string[];
@@ -15,29 +14,24 @@ export default function WordMatrix({ word, answer }: WordMatrixProps) {
           !isCorrectLetterAndPosition && answer.includes(letter) && letter !== '';
         const isWrongLetter = !isCorrectLetterAndPosition && !isCorrectLetterWrongPosition;
 
-        const backgroundColorClass = classnames({
+        {/*const backgroundColorClass = classnames({
           'bg-green-500': isCorrectLetterAndPosition,
           'bg-yellow-300': isCorrectLetterWrongPosition,
           'bg-slate-50': isWrongLetter || letter === '' || !letter,
-        });
+        })*/}
+
+        const backgroundColorClass = 
+        `flex justify-center items-center text-4xl font-normal p-0 border shadow w-16 h-16 
+        ${isCorrectLetterAndPosition ? 'bg-green-500 ' : ''}
+        ${isCorrectLetterWrongPosition ? 'bg-yellow-300 ' : ''}
+        ${isWrongLetter || letter === '' || !letter ? 'bg-slate-50' : ''}`;
+
         
         
         return (
           <div
             key={index}
-            className={classnames(
-              'flex',
-              'justify-center',
-              'items-center',
-              'text-4xl', 
-              'font-normal',
-              'p-0',
-              'border',
-              'shadow',
-              'w-16',
-              'h-16',
-              backgroundColorClass,
-            )}
+            className={backgroundColorClass}
           >
             {letter.toUpperCase()}
           </div>
