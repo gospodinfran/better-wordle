f70d574ca8068f58bd38f6d185163f64b55a25f1
Fran <kezicfran@gmail.com>
2023-05-31 22:53:34 +0300

    improve header appearance

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,14 @@ import BasicSettings from './BasicSettings';
 export default function Header() {
 
     return (
-        <div className='flex justify-between p-4 border border-gray-300 mb-10'>
+        <div className='flex justify-between items-center p-3 border-b border-gray-200 mb-10 text-5xl'>
 
-        <MenuIcon fontSize='large' className='hover:cursor-pointer ml-2' />
+        <MenuIcon fontSize='inherit' className='hover:cursor-pointer ml-2
+         hover:bg-slate-100 p-1 rounded-lg' />
         <h1 className="font-mono text-3xl font-semibold">Wordle</h1>
         <BasicSettings>
-        <SettingsIcon fontSize='large' className='hover:cursor-pointer mr-2' />
+        <SettingsIcon fontSize='inherit' className='hover:cursor-pointer mr-2 
+         hover:bg-slate-100 p-1 rounded-lg' />
         </BasicSettings>
 
         </div>
